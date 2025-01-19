6a93b2c95910b945a21490d197b9690190db2ffc
adrieeltorres1 <adrieltorreslds@gmail.com>
2025-01-18 22:39:14 -0300

    Lua completa

--- index.js
+++ index.js
@@ -1,20 +1,42 @@
 let numeroGerado = parseInt(Math.random() * 100);
+console.log(numeroGerado);
 let tentativas = [];
 
 numero.onchange = () => {
-    if (tentativas.length < 10) {
-        if (tentativas.length > 0) {
+    let numeroDigitado = Number(numero.value);
+    if(tentativas.length < 10){
+        if(tentativas.length > 0){
             for(let i = 0; i < tentativas.length; i++){
-                if(tentativas[i] == Number(numero.value)){
-                    alert("Este número já foi digitado");
+                if(tentativas[i] == numeroDigitado){
+                    alert("Este número ja foi digitado");
                     numero.value = '';
                     return;
                 }
-            }  
+            }
         }
-        tentativas.push(Number(numero.value));
+        tentativas.push(numeroDigitado);
+        if(numeroDigitado > numeroGerado){
+            alert('Bia esta pensando em numero menor')
+        }else if(numeroDigitado < numeroGerado){
+            alert('Bia esta pensando em numero maior')
+        }else{
+            mensagem.innerHTML = `Parabéns o número é: ${numeroGerado}`;
+            resultado.style.display = 'block';
+            numero.disabled = true;
+            bia.src = "bia-sorrindo.png";
+            reiniciar.innerText = 'Jogar novamente';
+            reiniciar.style.display = 'block';
+        }
         numero.value = '';
         palpites.innerHTML = tentativas.join('-');
+    } else {
+        bia.src = "bia-chorando.png";
+        mensagem.innerHTML = `Você não acertou o número era: ${numeroGerado}`;
+        resultado.style.display = 'block';
+        reiniciar.style.display = 'block';
     }
 }
 
+reiniciar.onclick = () => {
+    window.location.reload();
+}
